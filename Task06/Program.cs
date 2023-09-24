ea62fe6a30774a0a5d257b383ff98a966559241b
Shamil <s.butaev.gb.r>
2023-09-24 08:47:17 +0300

    task06

---
+++ Task06/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter your number");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int n2 = n % 2;
+
+if(n2 == 0)
+{
+    Console.WriteLine("Your number is even!");
+}
+else
+{
+    Console.WriteLine("Your number is odd");
+}
