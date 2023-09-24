93881440933a1140bbb7d3a3ef118cac5254626a
Shamil <s.butaev.gb.r>
2023-09-24 08:21:59 +0300

    Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Enter your first number");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your second number");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your third number");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+int max = n1;
+
+if(n2>n1)
+{
+max = n2; 
+}
+if (n3>n2)
+{
+    max = n3;
+}
+else if(n3>n1)
+{
+    max = n3;
+}
+Console.WriteLine($"Your highest number is {max}");
