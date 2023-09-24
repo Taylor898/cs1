72dab66352190750e70cc89a90321f2f90b0d4ea
Shamil <s.butaev.gb.r>
2023-09-24 08:12:12 +0300

    t2

---
+++ Task02/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter your first number");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your second number");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if(n>n2)
+{
+    Console.WriteLine($"{n} is larger than {n2}");
+}
+if(n<n2)
+{
+    Console.WriteLine($"{n} is smaller than {n2}");
+}
+else if(n == n2)
+{
+    Console.WriteLine($"{n} is equal to {n2}");
+}
